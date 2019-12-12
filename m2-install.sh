e065f65d0eb8b2c06c57b89d6533c0d59de5bf8f
Nemanja Djuric <ne@nemanja.io>
2019-12-12 23:59:08 +0100

    Update m2-install.sh

--- m2-install.sh
+++ m2-install.sh
@@ -2,7 +2,7 @@
 cd /workspace/magento2gitpod &&
 tar -xvf magento-2.3.3.tar.gz &&
 mysql -e 'create database nemanja;' &&
-url=$(gp url)
+url=$(gp url) &&
 url+="/"
 
 php bin/magento setup:install --db-name='nemanja' --db-user='root' --db-password='' --base-url=$url --backend-frontname='admin' --admin-user='admin' --admin-password='adm4540' --admin-email='ne@nemanja.io' --admin-firstname='Nemanja' --admin-lastname='Djuric' --use-rewrites='1' --use-secure='1' --base-url-secure=$url --use-secure-admin='1' --language='en_US' --db-host='localhost' --cleanup-database --timezone='America/New_York' --currency='USD' --session-save='files'
