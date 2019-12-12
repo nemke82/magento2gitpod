2060ac99caa8ab2d34c50e883c6e9ac819c3ec78
Nemanja Djuric <ne@nemanja.io>
2019-12-12 18:47:21 +0100

    Update blackfire-run.sh

--- blackfire-run.sh
+++ blackfire-run.sh
@@ -1,22 +1,12 @@
 #!/bin/bash
 
-variables=( "BLACKFIRE_SERVER_ID" "BLACKFIRE_SERVER_TOKEN" "BLACKFIRE_CLIENT_ID" "BLACKFIRE_CLIENT_TOKEN" "BLACKFIRE_SOCKET" "BLACKFIRE_LOG_LEVEL" "BLACKFIRE_LOG_FILE")
-
-for var in "${variables[@]}"
-do
-   :
-   sed -i "s|%$var%|${!var}|g" /etc/blackfire/agent
-   sed -i "s|%$var%|${!var}|g" /etc/php/7.2/fpm/conf.d/92-blackfire-config.ini
-   sed -i "s|%$var%|${!var}|g" /etc/php/7.2/cli/conf.d/92-blackfire-config.ini
-done
-
 blackfire-agent --register
-/etc/init.d/blackfire-agent restart
-
 blackfire config
 
 cat /etc/blackfire/agent | grep "server-id=" | sed 's/server-id/blackfire.server_id/' >> /etc/php/7.2/cli/conf.d/92-blackfire-config.ini
 cat /etc/blackfire/agent | grep "server-token=" | sed 's/server-token/blackfire.server_token/' >> /etc/php/7.2/cli/conf.d/92-blackfire-config.ini
+
+blackfire-agent --socket="unix:///tmp/agent.sock" &
 
 #restart php7.2-fpm
 service php7.2-fpm reload && service php7.2-fpm restart
