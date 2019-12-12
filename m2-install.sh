a66bb99759af57ac8a6e7fce60e24fac2889d76d
Nemanja Djuric <ne@nemanja.io>
2019-12-12 23:53:48 +0100

    Update m2-install.sh

--- m2-install.sh
+++ m2-install.sh
@@ -5,7 +5,7 @@ mysql -e 'create database nemanja;' &&
 url=$(gp url)
 url+="/"
 
-php bin/magento setup:install --db-name='nemanja' --db-user='root' --db-password='' --base-url='$url' --backend-frontname='admin' --admin-user='admin' --admin-password='adm4540' --admin-email='ne@nemanja.io' --admin-firstname='Nemanja' --admin-lastname='Djuric' --use-rewrites='1' --use-secure='1' --base-url-secure='$url' --use-secure-admin='1' --language='en_US' --db-host='localhost' --cleanup-database --timezone='America/New_York' --currency='USD' --session-save='files'
+php bin/magento setup:install --db-name='nemanja' --db-user='root' --db-password='' --base-url=$url --backend-frontname='admin' --admin-user='admin' --admin-password='adm4540' --admin-email='ne@nemanja.io' --admin-firstname='Nemanja' --admin-lastname='Djuric' --use-rewrites='1' --use-secure='1' --base-url-secure=$url --use-secure-admin='1' --language='en_US' --db-host='localhost' --cleanup-database --timezone='America/New_York' --currency='USD' --session-save='files'
 
 echo "Click here and then Open Browser button  --------------------------------------------------------------"
 echo "                                                                                                      |"
