83223b064f49ae72b19c85190e4b2c975553fe70
Nemanja Djuric <ne@nemanja.io>
2020-10-25 22:26:47 +0100

    Update m2-install-solo.sh

--- m2-install-solo.sh
+++ m2-install-solo.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 cd /workspace/magento2gitpod &&
 git clone https://github.com/magento/magento2.git && cd magento2 && cp -avr .* /workspace/magento2gitpod && cd .. && rm -r -f magento2 &&
-mysql -e 'create database nemanja;' &&
+
 url=$(gp url | awk -F"//" {'print $2'}) && url+="/" && url="https://8002-"$url
 
 redis-server &
-
+mysql -e 'create database nemanja;' &&
 composer install && php bin/magento setup:install --db-name='nemanja' --db-user='root' --db-password='nem4540' --base-url=$url --backend-frontname='admin' --admin-user='admin' --admin-password='adm4540' --admin-email='ne@nemanja.io' --admin-firstname='Nemanja' --admin-lastname='Djuric' --use-rewrites='1' --use-secure='1' --base-url-secure=$url --use-secure-admin='1' --language='en_US' --db-host='127.0.0.1' --cleanup-database --timezone='America/New_York' --currency='USD' --session-save='redis'
 
 yes | php bin/magento setup:config:set --session-save=redis --session-save-redis-host=127.0.0.1 --session-save-redis-log-level=3 --session-save-redis-db=0 --session-save-redis-port=6379 &&
