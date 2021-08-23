4113ff45a270dbef645a3c4567d9d3841aac37d0
Nemanja Djuric <ne@nemanja.io>
2021-08-23 20:27:20 +0200

    Update m2-install.sh

--- m2-install.sh
+++ m2-install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sudo composer selfupdate --2;
 cd /workspace/magento2gitpod &&
 composer create-project --repository-url=https://repo.magento.com/ magento/project-community-edition=2.4.3 magento2
 cd magento2 && cp -avr .* /workspace/magento2gitpod;
@@ -10,7 +11,6 @@ url=$(gp url | awk -F"//" {'print $2'}) && url+="/" && url="https://8002-"$url;
 redis-server &
 $ES_HOME79/bin/elasticsearch -d -p $ES_HOME79/pid -Ediscovery.type=single-node &
 sleep 15;
-sudo composer selfupdate --2;
 
 mysql -u root -pnem4540 -e 'create database nemanja;' &&
 url=$(gp url | awk -F"//" {'print $2'}) && url+="/" && url="https://8002-"$url;cd /workspace/magento2gitpod && composer install && php bin/magento setup:install --db-name='nemanja' --db-user='root' --db-password='nem4540' --base-url=$url --backend-frontname='admin' --admin-user='admin' --admin-password='adm4540' --admin-email='ne@nemanja.io' --admin-firstname='Nemanja' --admin-lastname='Djuric' --use-rewrites='1' --use-secure='1' --base-url-secure=$url --use-secure-admin='1' --language='en_US' --db-host='127.0.0.1' --cleanup-database --timezone='America/New_York' --currency='USD' --session-save='redis' --amqp-host="127.0.0.1" --amqp-port="5672" --amqp-user="guest" --amqp-password="guest" --amqp-virtualhost="/"
