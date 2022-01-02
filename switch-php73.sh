7b5a26fd8ae100fda18aad9a3b79cf8b983413b5
Nemanja Djuric <ne@nemanja.io>
2022-01-02 13:51:58 +0100

    Update switch-php73.sh

--- switch-php73.sh
+++ switch-php73.sh
@@ -12,6 +12,7 @@ sudo apt-get -y autoremove;
 sudo apt-get clean;
 sudo rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*;
 sudo update-alternatives --set php /usr/bin/php7.3;
-sudo cp php-fpm73.conf /etc/php/7.3/fpm/php-fpm.conf
+sudo cp php-fpm73.conf /etc/php/7.3/fpm/php-fpm.conf;
+sudo chown gitpod:gitpod /tmp/php7.3-fpm.log;
 sudo /etc/init.d/php7.4-fpm stop;
 sudo /etc/init.d/php7.3-fpm start
