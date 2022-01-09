cbcaa8b3e47accd2445d593fc323b2f0951d8fac
Nemanja Djuric <ne@nemanja.io>
2022-01-09 18:37:38 +0100

    fix for composer 2.2
    Added --no-interaction --no-progress to composer require and composer install due https://github.com/magento/magento2/issues/34831

--- m2-install-solo.sh
+++ m2-install-solo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Clone M2 repository (dev), for composer comment these lines and use following
-# composer create-project --repository=https://repo.magento.com/ magento/project-community-edition magento2
+# composer create-project --no-interaction --no-progress --repository=https://repo.magento.com/ magento/project-community-edition magento2
 sudo composer selfupdate --2;
 cd /workspace/magento2gitpod &&
 git clone https://github.com/magento/magento2.git && cd magento2 && cp -avr .* /workspace/magento2gitpod &&
@@ -17,7 +17,7 @@ $ES_HOME79/bin/elasticsearch -d -p $ES_HOME79/pid -Ediscovery.type=single-node &
 sleep 15;
 
 mysql -u root -pnem4540 -e 'create database nemanja;' &&
-url=$(gp url | awk -F"//" {'print $2'});url+="/";url="https://8002-"$url;cd /workspace/magento2gitpod && composer install && php bin/magento setup:install --db-name='nemanja' --db-user='root' --db-password='nem4540' --base-url=$url --backend-frontname='admin' --admin-user='admin' --admin-password='adm4540' --admin-email='ne@nemanja.io' --admin-firstname='Nemanja' --admin-lastname='Djuric' --use-rewrites='1' --use-secure='1' --base-url-secure=$url --use-secure-admin='1' --language='en_US' --db-host='127.0.0.1' --cleanup-database --timezone='America/New_York' --currency='USD' --session-save='redis' --amqp-host="127.0.0.1" --amqp-port="5672" --amqp-user="guest" --amqp-password="guest" --amqp-virtualhost="/"
+url=$(gp url | awk -F"//" {'print $2'});url+="/";url="https://8002-"$url;cd /workspace/magento2gitpod && composer install --no-interaction --no-progress && php bin/magento setup:install --db-name='nemanja' --db-user='root' --db-password='nem4540' --base-url=$url --backend-frontname='admin' --admin-user='admin' --admin-password='adm4540' --admin-email='ne@nemanja.io' --admin-firstname='Nemanja' --admin-lastname='Djuric' --use-rewrites='1' --use-secure='1' --base-url-secure=$url --use-secure-admin='1' --language='en_US' --db-host='127.0.0.1' --cleanup-database --timezone='America/New_York' --currency='USD' --session-save='redis' --amqp-host="127.0.0.1" --amqp-port="5672" --amqp-user="guest" --amqp-password="guest" --amqp-virtualhost="/"
 
 n98-magerun2 module:disable Magento_Csp &&
 n98-magerun2 module:disable Magento_TwoFactorAuth &&
