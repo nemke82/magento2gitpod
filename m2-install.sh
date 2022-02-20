9e53de5fd7dfb874d39e94240268589ec60aa171
Nemanja Djuric <ne@nemanja.io>
2022-02-20 19:07:22 +0100

    fix composer ownership after change

--- m2-install.sh
+++ m2-install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sudo composer selfupdate --2;
+sudo chown -R gitpod:gitpod /home/gitpod/.config/composer;
 cd /workspace/magento2gitpod &&
 composer config -g -a http-basic.repo.magento.com 64229a8ef905329a184da4f174597d25 a0df0bec06011c7f1e8ea8833ca7661e &&
 composer create-project --no-interaction --no-progress --repository-url=https://repo.magento.com/ magento/project-community-edition=2.4.3 magento2
