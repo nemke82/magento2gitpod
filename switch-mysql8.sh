42394702b38caa1d9cc145d27e97034516fb2fd3
nemke82 <ne@nemanja.io>
2024-06-29 17:38:59 +0200

    Introduction PHP 8.3, Removal of Shell Menu manager, Flask Menu Manager - fix #9

--- switch-mysql8.sh
+++ switch-mysql8.sh
@@ -2,7 +2,15 @@
 
 export DEBIAN_FRONTEND=noninteractive
 
+# Stop MySQL service and supervisor
 sudo supervisorctl stop mysql
+
+# Temporarily move existing MySQL data directory
+if [ -d /var/lib/mysql ]; then
+    sudo mv /var/lib/mysql /var/lib/mysql.bak
+fi
+
+# Enable Percona release and import Google signing key
 sudo percona-release enable ps-80 release
 sudo wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sudo apt-get update -y
@@ -13,11 +21,16 @@ echo "percona-server-server percona-server-server/root_password_again password n
 echo "percona-server-server-8.0 percona-server-server/root-pass password nem4540" | sudo debconf-set-selections
 echo "percona-server-server-8.0 percona-server-server/re-root-pass password nem4540" | sudo debconf-set-selections
 echo "percona-server-server percona-server-server/default-auth-override select Use Legacy Authentication Method (Retain MySQL 5.x Compatibility)" | sudo debconf-set-selections
-echo "percona-server-server percona-server-server/data-dir-existing select Abort installation" | sudo debconf-set-selections
 
-# Install and upgrade Percona server packages
-sudo apt-get update -y
-sudo apt-get upgrade -y percona-server-server percona-server-client percona-server-common
+# Install Percona server packages
+sudo apt-get install -y percona-server-server percona-server-client percona-server-common
+
+# Restore the original data directory
+if [ -d /var/lib/mysql.bak ]; then
+    sudo rm -rf /var/lib/mysql
+    sudo mv /var/lib/mysql.bak /var/lib/mysql
+    sudo chown -R mysql:mysql /var/lib/mysql
+fi
 
 # Update MySQL configuration
 sed -i 's#query_cache_limit=2M##g' /etc/mysql/conf.d/mysqld.cnf
