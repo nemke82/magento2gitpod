9712a215f718e548e1563573d8be9350b1bf93cd
nemke82 <ne@nemanja.io>
2024-06-29 16:58:13 +0200

    Introduction PHP 8.3, Removal of Shell Menu manager, Flask Menu Manager - fix #6

--- switch-mysql8.sh
+++ switch-mysql8.sh
@@ -1,23 +1,27 @@
-sudo supervisorctl stop mysql;
-sudo percona-release enable ps-80 release;
+#!/bin/bash
+
+sudo supervisorctl stop mysql
+sudo percona-release enable ps-80 release
 sudo wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-sudo apt-get update -y;
-set -ex; \
-	{ \
-		for key in \
-			percona-server-server/root_password \
-			percona-server-server/root_password_again \
-			"percona-server-server-$PERCONA_MAJOR/root-pass" \
-			"percona-server-server-$PERCONA_MAJOR/re-root-pass" \
-		; do \
-			sudo echo "percona-server-server-$PERCONA_MAJOR" "$key" password 'nem4540'; \
-		done; \
-	} | sudo debconf-set-selections;
-sudo apt-get update;
-sudo apt-get upgrade -y percona-server-server percona-server-client percona-server-common;
-sed -i 's#query_cache_limit=2M##g' /etc/mysql/conf.d/mysqld.cnf;
-sed -i 's#query_cache_size=128M##g' /etc/mysql/conf.d/mysqld.cnf;
-sed -i 's#query_cache_type=1##g' /etc/mysql/conf.d/mysqld.cnf;
-sudo echo "default_authentication_plugin=mysql_native_password" >> /etc/mysql/conf.d/mysqld.cnf;
-sudo chown -R gitpod:gitpod /var/run/mysqld/;
+sudo apt-get update -y
+
+# Preconfigure debconf selections
+echo "percona-server-server percona-server-server/root_password password nem4540" | sudo debconf-set-selections
+echo "percona-server-server percona-server-server/root_password_again password nem4540" | sudo debconf-set-selections
+echo "percona-server-server-8.0 percona-server-server/root-pass password nem4540" | sudo debconf-set-selections
+echo "percona-server-server-8.0 percona-server-server/re-root-pass password nem4540" | sudo debconf-set-selections
+echo "percona-server-server percona-server-server/default-auth-override select Use Legacy Authentication Method (Retain MySQL 5.x Compatibility)" | sudo debconf-set-selections
+
+# Install and upgrade Percona server packages
+sudo apt-get update
+sudo apt-get upgrade -y percona-server-server percona-server-client percona-server-common
+
+# Update MySQL configuration
+sed -i 's#query_cache_limit=2M##g' /etc/mysql/conf.d/mysqld.cnf
+sed -i 's#query_cache_size=128M##g' /etc/mysql/conf.d/mysqld.cnf
+sed -i 's#query_cache_type=1##g' /etc/mysql/conf.d/mysqld.cnf
+echo "default_authentication_plugin=mysql_native_password" | sudo tee -a /etc/mysql/conf.d/mysqld.cnf
+
+# Set permissions and restart MySQL
+sudo chown -R gitpod:gitpod /var/run/mysqld/
 sudo supervisorctl start mysql
