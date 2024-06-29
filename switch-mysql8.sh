0f2c253f46f41c341e5af9c887160f82f0d11858
nemke82 <ne@nemanja.io>
2024-06-29 20:37:09 +0200

    Introduction PHP 8.3, Removal of Shell Menu manager, Flask Menu Manager - fix #16

--- switch-mysql8.sh
+++ switch-mysql8.sh
@@ -5,19 +5,14 @@ export PERCONA_MAJOR=8.0
 sudo supervisorctl stop mysql;
 sudo percona-release enable ps-80 release;
 sudo apt-get update -y;
-set -ex; \
-	{ \
-		for key in \
-			percona-server-server/root_password \
-			percona-server-server/root_password_again \
-			"percona-server-server-$PERCONA_MAJOR/root-pass" \
-			"percona-server-server-$PERCONA_MAJOR/re-root-pass" \
-            "percona-server-server percona-server-server/default-auth-override select Use Legacy Authentication Method (Retain MySQL 5.x Compatibility)" \
-            "percona-server-server-8.0 percona-server-server/use-exist-apparmor-profile select Use existent AppArmor profile (RECOMMENDED)" \
-		; do \
-			sudo echo "percona-server-server-$PERCONA_MAJOR" "$key" password 'nem4540'; \
-		done; \
-	} | sudo debconf-set-selections;
+
+# Preconfigure debconf selections for Percona installation
+echo "percona-server-server-8.0 percona-server-server/root_password password nem4540" | sudo debconf-set-selections
+echo "percona-server-server-8.0 percona-server-server/root_password_again password nem4540" | sudo debconf-set-selections
+echo "percona-server-server-8.0 percona-server-server/root-pass password nem4540" | sudo debconf-set-selections
+echo "percona-server-server-8.0 percona-server-server/re-root-pass password nem4540" | sudo debconf-set-selections
+echo "percona-server-server-8.0 percona-server-server/default-auth-override select Use Legacy Authentication Method (Retain MySQL 5.x Compatibility)" | sudo debconf-set-selections
+echo "percona-server-server-8.0 percona-server-server/use-exist-apparmor-profile select Use existent AppArmor profile (RECOMMENDED)" | sudo debconf-set-selections
 sudo apt-get update;
 sudo apt-get upgrade -y percona-server-server percona-server-client percona-server-common;
 sed -i 's#query_cache_limit=2M##g' /etc/mysql/conf.d/mysqld.cnf;
